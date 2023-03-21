f95dfe2b64656d63eca63ffb4cbad1935a9fed0d
nattyeyes <nathanamata@gmail.com>
2023-03-21 09:15:07 +0800

    Add activity code

---
+++ indexActivity.js
@@ -0,0 +1,18 @@
+console.log('Hello, Welcome to De La Salle University!');
+
+let	firstName = 'Juan';
+let	lastName = 'Cruz';
+let	age = 20;
+let	hobbies = ['Reading', 'Sports', 'Gaming', 'Guitar', 'Singing'];
+let	homeAddress = {
+	houseNumber: 123,
+	street: "Malunggay St.",
+	subDivision: "Golden City",
+	barangay: "Salawag",
+	city: "Dasmarinas",
+	province: "Cavite",
+	zipCode: 4114
+};
+
+console.log(`First Name: ${firstName} Last Name: ${lastName} Age: ${age}`);
+console.log(homeAddress);
